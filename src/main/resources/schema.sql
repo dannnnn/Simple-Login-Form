906f6430d9a01bb09875d672daebc6e43f76ec92
Ambalabada <rumbapumba6@gmail.com>
2018-10-25 15:36:36 +0300

    Fixed some issues

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,3 @@
--- Database: app_data
-
--- DROP DATABASE app_data;
-
-CREATE DATABASE app_data
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
 CREATE TABLE IF NOT EXISTS users (
     username    VARCHAR(20) PRIMARY KEY,
     password    VARCHAR(65) NOT NULL,
